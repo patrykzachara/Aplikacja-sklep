60642bec633440f204c69a499115faf115872548
patrykzachara <patryk.zachara@microsoft.wsei.edu.pl>
2022-07-06 12:57:46 +0200

    Services xml documentation

--- WPF/Services/ProductService.cs
+++ WPF/Services/ProductService.cs
@@ -13,21 +13,42 @@ namespace WPF.Services
         {
             _appContext = appContext;
         }
+        /// <summary>
+        /// Getting products from database
+        /// </summary>
+        /// <param name="shopId">Shop id</param>
+        /// <returns>Products from shop</returns>
         public IEnumerable<Product> GetProducts(int shopId)
         {
             var products = _appContext.Products.Where(x => x.ShopId == shopId);
             return products;
         }
+        /// <summary>
+        /// Getting product from database
+        /// </summary>
+        /// <param name="productid">product id</param>
+        /// <param name="shopId">shop id</param>
+        /// <returns>Product</returns>
         public Product GetProductBiId(int productid, int shopId)
         {
             return _appContext.Products.Where(x => x.ShopId == shopId).Single(x => x.Id == productid);
         }
+        /// <summary>
+        /// Adding product to database
+        /// </summary>
+        /// <param name="product">Product model to add</param>
         public void AddProduct(Product product)
         {
             product.LastUpdateTime = DateTime.UtcNow;
             _appContext.Products.Add(product);
             _appContext.SaveChanges();
         }
+        /// <summary>
+        /// Updating products quantity
+        /// </summary>
+        /// <param name="productid">product id</param>
+        /// <param name="shopId">shop id</param>
+        /// <param name="newQuantity">new quantity</param>
         public void UpdateQuantity(int productid, int shopId, int newQuantity)
         {
             var product = GetProductBiId(productid, shopId);
@@ -36,6 +57,12 @@ namespace WPF.Services
             _appContext.Products.Update(product);
             _appContext.SaveChanges();
         }
+        /// <summary>
+        /// Updating product price
+        /// </summary>
+        /// <param name="productid">product id</param>
+        /// <param name="shopId">shop id</param>
+        /// <param name="newQuantity">new quantity</param>
         public void UpdatePrice(int productid, int shopId, decimal newPrice)
         {
             var product = GetProductBiId(productid, shopId);
--- WPF/Services/ShopService.cs
+++ WPF/Services/ShopService.cs
@@ -16,15 +16,28 @@ namespace WPF.Services
         {
             _context = context;
         }
+        /// <summary>
+        /// Gettong shops from database
+        /// </summary>
+        /// <returns>shops</returns>
         public IEnumerable<Shop> GetShops()
         {
             return _context.Shops.Include(x => x.Adress);
         }
+        /// <summary>
+        /// Adding shop to database
+        /// </summary>
+        /// <param name="shop">Shop model to add</param>
         public void AddShop(Shop shop)
         {
             _context.Shops.Add(shop);
             _context.SaveChanges();
         }
+        /// <summary>
+        /// Getting shop model from database
+        /// </summary>
+        /// <param name="id">Shop id</param>
+        /// <returns>Shop model</returns>
         public Shop GetShopById(int id)
         {
             return _context.Shops.Include(x => x.Adress).Where(x => x.Id == id).Single();
